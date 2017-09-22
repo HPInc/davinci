d01bcbfccc18915d450cc0dba7dd6435e11457de
Jonathan Lambert <jonathanprl@users.noreply.github.com>
2017-09-22 14:14:21 +0100

    Merge pull request #25 from Oneflow/feat/feather-service-options
    feat: added options override to mongoose service

--- packages/of-base-api/src/MongooseModel.js
+++ packages/of-base-api/src/MongooseModel.js
@@ -3,14 +3,15 @@ const service = require('feathers-mongoose');
 const { applyHook } = require('./hookUtils');
 
 class MongooseModel {
-	constructor(name, schema, collection) {
+	constructor(name, schema, collection, options = {}) {
 		this.Schema = schema instanceof mongoose.Schema ? schema : new mongoose.Schema(schema);
 		this.Model = mongoose.model(name, this.Schema, collection);
 
 		const featherService = service({
 			Model: this.Model,
+			paginate: false,
 			lean: true, // set to false if you want Mongoose documents returned
-			paginate: false
+			...options
 		});
 
 		return applyHook(featherService);
