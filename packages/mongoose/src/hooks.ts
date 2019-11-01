082326f5584d3251baabc7929f0a7276ae6cfefa
Piero Maltese <maltesepiero@gmail.com>
2019-11-01 15:15:43 +0000

    fixed tests

--- packages/mongoose/src/hooks.ts
+++ packages/mongoose/src/hooks.ts
@@ -32,6 +32,9 @@ const createRegisterHooks = (hooksList, stage) => (mongooseSchema, handler) => {
 
 	return hooksList.forEach(hook =>
 		mongooseSchema[stage](hook, async function() {
+			if (this.setOptions) {
+				this.setOptions({ runValidators: true });
+			}
 			const args = [hook];
 			if (hasContextInOptions(hook)) {
 				args.unshift(this.options.context);
