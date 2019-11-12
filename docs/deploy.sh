4e6451a880622b57c00374677850e2f4cf3c0728
Piero Maltese <maltesepiero@gmail.com>
2019-11-12 09:58:17 +0000

    deploy script amends

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -9,14 +9,16 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 npm rebuild node-sass
 npm run docs:build
 
+git config --global user.email "circleci@hp.com"
+git config --global user.name "Circle CI"
+git checkout
+git add docs/.vuepress/dist/
+git checkout gh-pages
+
 ls | grep -v docs | xargs rm -r
 cp -rlf ./docs/.vuepress/dist/* ./
 rm -r ./docs
 
-git config --global user.email "circleci@hp.com"
-git config --global user.name "Circle CI"
-git checkout gh-pages
-git add -A
-git pull
 git commit --allow-empty -am "Update Docs"
+git pull
 git push
