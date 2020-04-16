6c3cdecbc31aad90c9c0ea3085bf134a7558eb49
Piero Maltese <maltesepiero@gmail.com>
2020-04-16 11:22:13 +0100

    docs(hooks): updated documentation

--- examples/crm/src/api/customer/customer.model.ts
+++ examples/crm/src/api/customer/customer.model.ts
@@ -3,26 +3,38 @@ import { mgoose } from '@davinci/mongoose';
 import CustomerSchema from './customer.schema';
 import { Context } from '../../types';
 
-const { generateSchema, beforeRead, beforeWrite, beforeDelete } = mgoose;
+const { generateSchema, beforeRead, beforeWrite, afterDelete } = mgoose;
 
 const schema = generateSchema(CustomerSchema);
 
 beforeRead<Context>(schema, ({ query, context }) => {
-	if (context) {
-		const currentQuery = query.getQuery();
-		query.setQuery({ ...currentQuery, accountId: context.accountId });
-	}
-});
-
-beforeWrite<Context, CustomerSchema>(schema, ({ doc, context }) => {
 	// inject accountId before persisting into DB
-	if (context) {
+	if (!context) return;
+
+	const currentQuery = query.getQuery();
+	query.setQuery({ ...currentQuery, accountId: context.accountId });
+});
+
+beforeWrite<Context, CustomerSchema>(schema, ({ query, doc, context }) => {
+	// inject accountId before persisting into DB
+	if (!context) return;
+
+	// required check for atomic operations
+	if (doc) {
 		doc.accountId = context.accountId;
+	} else {
+		// @ts-ignore
+		query.setUpdate({
+			...query.getUpdate(),
+			accountId: context.accountId
+		});
 	}
 });
 
-beforeDelete(schema, () => {
-	// perform some cleanup
+afterDelete<Context>(schema, ({ doc }) => {
+	if (doc) {
+		// perform some cleanup
+	}
 });
 
 const Customer = model<CustomerSchema & Document>('Customer', schema, 'customers');
