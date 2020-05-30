ea3d7b4de0ca46ccc832fb3d0772b2d3dc277c13
Piero Maltese <maltesepiero@gmail.com>
2020-05-30 21:23:59 +0100

    fix: controller middlewares have precedence

--- packages/graphql/test/unit/decorators/index.test.ts
+++ packages/graphql/test/unit/decorators/index.test.ts
@@ -6,17 +6,22 @@ describe('decorators', () => {
 		it('should add the middleware functions in the right order', () => {
 			const firstMw = () => {};
 			const secondMw = () => {};
+			const thirdMw = () => {};
 
 			@graphql.middleware(firstMw)
 			class Base {}
 
 			@graphql.middleware(secondMw)
-			class Controller extends Base {}
+			class Controller extends Base {
+				@graphql.middleware(thirdMw)
+				method() {}
+			}
 
 			const middlewares = Reflector.getMetadata('davinci:graphql:middleware', Controller);
 
 			middlewares[0].middlewareFunction.should.be.equal(firstMw);
 			middlewares[1].middlewareFunction.should.be.equal(secondMw);
+			middlewares[2].middlewareFunction.should.be.equal(thirdMw);
 		});
 	});
 });
