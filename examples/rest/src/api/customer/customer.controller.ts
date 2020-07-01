6d4d9baf3755c485348db968f1ef7823357c9fd4
Piero Maltese <maltesepiero@gmail.com>
2020-07-01 14:10:03 +0100

    rolled back rest example

--- examples/rest/src/api/customer/customer.controller.ts
+++ examples/rest/src/api/customer/customer.controller.ts
@@ -1,6 +1,9 @@
-import { express, route } from '@davinci/core';
+import { context, express, route } from '@davinci/core';
+import { createMongooseController } from '@davinci/mongoose';
+import model from './customer.model';
+import CustomerSchema from './customer.schema';
 
-const { post, controller, body } = route;
+const { get, controller, query } = route;
 
 @controller({
 	basepath: '/api/customers'
@@ -9,10 +12,34 @@ const { post, controller, body } = route;
 	console.log('controller before middleware');
 	next();
 })
-export default class CustomerController {
-	@post({ path: '/hello', summary: 'That is a hello method' })
-	hello(@body({ type: [String] }) firstnames: string[]) {
-		console.log(firstnames);
-		return firstnames;
+export default class CustomerController extends createMongooseController(model, CustomerSchema) {
+	@get({ path: '/hello', summary: 'That is a hello method' })
+	hello(@query() firstname: string, @query() age: number, @context() context) {
+		console.log(firstname, age, context);
+		return firstname;
+	}
+
+	@get({ path: '/customResponse', summary: 'That is a custom response method' })
+	customResponse(@express.res() res) {
+		res.json({ test: 1 });
+	}
+
+	@get({ path: '/customMiddlewares', summary: 'This is a method with custom middlewares' })
+	@express.middleware.before((req, res, next) => {
+		console.log(req, res);
+		next();
+	})
+	@express.middleware.after((req, res, next) => {
+		console.log(req, res);
+		next();
+	})
+	pathWithCustomMiddleware() {
+		return { result: 1 };
+	}
+
+	@get({ path: '/customHeader', summary: 'This is a method with custom header' })
+	@express.header('Content-Type', 'text/plan')
+	customHeader() {
+		return { result: 1 };
 	}
 }
