589442e49700d93d11e6f8ea63bdc7d9e0b6a2fd
Piero Maltese <maltesepiero@gmail.com>
2021-07-06 13:35:39 +0100

    chore: more tests

--- packages/graphql/test/unit/createGraphQLServer.test.ts
+++ packages/graphql/test/unit/createGraphQLServer.test.ts
@@ -38,12 +38,29 @@ describe('server instantiation', () => {
 			// verify that the middleware is in the express stack
 			const route = app._router.stack.find(({ name }) => name === 'graphqlMiddleware');
 			should(route).be.ok();
+		});
+
+		it('should accept handle GraphQL queries', async () => {
+			class Customer {
+				@graphql.field()
+				firstname: string;
+				@graphql.field()
+				age: number;
+			}
+
+			class CustomerController {
+				@graphql.query([Customer])
+				customers() {
+					return [{ firstname: 'John', age: 20 }];
+				}
+			}
+
+			createGraphQLServer(app, [CustomerController]);
 
 			// test the endpoint
 			const response = await supertest(app)
 				.post('/graphql')
-				.send({ query: 'query{customers { firstname age }}' })
-				.expect(200);
+				.send({ query: 'query{customers { firstname age }}' });
 
 			response.body.should.be.deepEqual({
 				data: {
@@ -53,6 +70,45 @@ describe('server instantiation', () => {
 							age: 20
 						}
 					]
+				}
+			});
+			response.statusCode.should.be.equal(200);
+		});
+
+		it('should accept handle GraphQL mutations', async () => {
+			class Customer {
+				@graphql.field()
+				firstname: string;
+				@graphql.field()
+				age: number;
+			}
+
+			class CustomerController {
+				@graphql.mutation(Customer)
+				uppercaseName(@graphql.arg() customer: Customer) {
+					return { ...customer, firstname: customer.firstname.toUpperCase() };
+				}
+
+				@graphql.query(String)
+				myQuery() {}
+			}
+
+			createGraphQLServer(app, [CustomerController]);
+
+			// test the endpoint
+			const response = await supertest(app)
+				.post('/graphql')
+				.send({
+					query: 'mutation { uppercaseName( customer: { firstname: "John", age: 20 } ) { firstname age }}',
+					variables: {}
+				});
+
+			response.body.should.be.deepEqual({
+				data: {
+					uppercaseName: {
+						age: 20,
+						firstname: 'JOHN'
+					}
 				}
 			});
 			response.statusCode.should.be.equal(200);
