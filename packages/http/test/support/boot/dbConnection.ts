cc264b9519648744b241f247c7edfbd8b3d0725e
Piero Maltese <maltesepiero@gmail.com>
2022-03-22 11:25:06 +0000

    chore: removed tests

--- packages/http/test/support/boot/dbConnection.ts
+++
@@ -1,8 +0,0 @@
-// const mongoose = require('mongoose');
-
-module.exports = () => {
-	// mongoose.Promise = global.Promise;
-	// mongoose.set('debug', true);
-	// const mongodbUrl = process.env.MONGODB_URL || 'mongodb://localhost/db';
-	// return mongoose.connect(mongodbUrl, { useNewUrlParser: true });
-};
