abe09cd9d64e3d96530090c941367c33fc87249d
Piero Maltese <maltesepiero@gmail.com>
2022-03-22 11:19:56 +0000

    feat(next): starting from scratch

--- packages/core/src/config.ts
+++
@@ -1,9 +0,0 @@
-/*
- * © Copyright 2020 HP Development Company, L.P.
- * SPDX-License-Identifier: MIT
- */
-
-export default {
-	PORT: process.env.PORT || 3000,
-	PROTOCOL: process.env.NODE_ENV === 'local' ? 'http' : 'https'
-};
--- packages/reflector/src/index.ts
+++
@@ -1,7 +0,0 @@
-/*
- * © Copyright 2020 HP Development Company, L.P.
- * SPDX-License-Identifier: MIT
- */
-
-export { default as Reflector } from './Reflector';
-export * from './shared-types';
