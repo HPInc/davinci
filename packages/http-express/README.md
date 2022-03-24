ba33b842808511965fefa49c243e20a96a39a88e
Piero Maltese <maltesepiero@gmail.com>
2022-03-24 16:57:44 +0000

    feat: renamed HttpModule to HttpServerModule, ExpressHttpModule to ExpressHttpServer
    - also added HttpServerModule unit tests

--- packages/http-express/README.md
+++
@@ -1,4 +0,0 @@
-## @davinci/http-express
-
-
-*Checkout [https://hpinc.github.io/davinci/](https://hpinc.github.io/davinci/)*
--- packages/http/src/index.ts
+++
@@ -1,7 +0,0 @@
-/*
- * © Copyright 2022 HP Development Company, L.P.
- * SPDX-License-Identifier: MIT
- */
-
-export * from './HttpModule';
-export * from './types';
