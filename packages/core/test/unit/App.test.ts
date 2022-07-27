99e96d623df84228f9745cf1124739633d16fd2b
Piero Maltese <maltesepiero@gmail.com>
2022-07-27 17:10:15 +0100

    tests: fixed typo

--- packages/core/test/unit/App.test.ts
+++ packages/core/test/unit/App.test.ts
@@ -170,7 +170,7 @@ describe('App', () => {
 			expect(err.message).to.match(/A module with the same identifier (.+) has already been registered/);
 		}
 	});
-	v;
+
 	it("should execute the modules' onInit hook", async () => {
 		const app = createApp();
 		class MyModule implements Module {
