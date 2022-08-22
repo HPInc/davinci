f2685a7b5210962fc6abb8928e54f9bcab72b7e5
Piero Maltese <maltesepiero@gmail.com>
2022-08-22 09:42:24 +0200

    [next] feat(lifecycle): added onRegister lifecycle hook for modules (#128)
    * feat(lifecycle): added onRegister lifecycle hook for modules
    
    * fix: fixed tests
    
    * test: wait for registration complete

--- packages/core/test/unit/App.test.ts
+++ packages/core/test/unit/App.test.ts
@@ -34,9 +34,48 @@ describe('App', () => {
 				throw new Error('Error within MyModule');
 			}
 		}
-		const app = createApp().registerModule([new MyModule()]);
+		const app = createApp();
+		await app.registerModule([new MyModule()]);
 
 		await expect(app.init()).to.be.rejectedWith('Error within MyModule');
+	});
+
+	it('should wait for the modules registration to be complete, before initializing', async () => {
+		const completionOrder = [];
+		class MyModule implements Module {
+			getModuleId() {
+				return 'myModule';
+			}
+
+			async onRegister() {
+				await new Promise(resolve => setTimeout(() => resolve(null), 200));
+				completionOrder.push('onRegister');
+			}
+
+			async onInit() {
+				completionOrder.push('onInit');
+			}
+		}
+		const app = createApp();
+		app.registerModule([new MyModule()]);
+		await app.init();
+
+		expect(completionOrder).to.be.deep.equal(['onRegister', 'onInit']);
+	});
+
+	it('should throw and exception and exit if an error happens during a module registration', async () => {
+		class MyModule implements Module {
+			getModuleId() {
+				return 'myModule';
+			}
+
+			onRegister() {
+				throw new Error('Error within MyModule');
+			}
+		}
+		const app = createApp();
+
+		await expect(app.registerModule([new MyModule()])).to.be.rejectedWith('Error within MyModule');
 	});
 
 	it('should ignore exceptions on modules happening on destroy', async () => {
@@ -49,7 +88,8 @@ describe('App', () => {
 				throw new Error('Error within MyModule');
 			}
 		}
-		const app = createApp().registerModule([new MyModule()]);
+		const app = createApp();
+		await app.registerModule([new MyModule()]);
 		const moduleLoggerErrorSpy = sinon.spy(app.logger, 'error');
 		const appLoggerFatalSpy = sinon.spy(app.logger, 'fatal');
 		await app.init();
@@ -163,7 +203,7 @@ describe('App', () => {
 			}
 		}
 		const myModule = new MyModule();
-		app.registerModule(myModule);
+		await app.registerModule(myModule);
 
 		expect(app.getModules()[0]).to.be.equal(myModule);
 	});
@@ -185,7 +225,7 @@ describe('App', () => {
 
 		const myModule1 = new MyModule1();
 		const myModule2 = new MyModule2();
-		app.registerModule([myModule1, myModule2]);
+		await app.registerModule([myModule1, myModule2]);
 
 		expect(app.getModules()).to.be.deep.equal([myModule1, myModule2]);
 	});
@@ -205,7 +245,7 @@ describe('App', () => {
 		const myModule2 = new MyModule2();
 
 		try {
-			app.registerModule([myModule1, myModule2]);
+			await app.registerModule([myModule1, myModule2]);
 			throw new Error('failed test');
 		} catch (err) {
 			expect(err.message).to.match(/A module with the same identifier (.+) has already been registered/);
@@ -225,7 +265,7 @@ describe('App', () => {
 			}
 		}
 		const myModule = new MyModule();
-		app.registerModule(myModule);
+		await app.registerModule(myModule);
 		await app.init();
 
 		expect(myModule.app).to.be.equal(app);
@@ -244,9 +284,28 @@ describe('App', () => {
 			}
 		}
 		const myModule = new MyModule();
-		app.registerModule(myModule);
+		await app.registerModule(myModule);
 		await app.init();
 		await app.shutdown();
+
+		expect(myModule.app).to.be.equal(app);
+	});
+
+	it("should execute the modules' onRegister hook", async () => {
+		const app = createApp();
+		class MyModule implements Module {
+			app: App;
+			getModuleId() {
+				return 'myModule';
+			}
+
+			onRegister(app: App) {
+				this.app = app;
+			}
+		}
+		const myModule = new MyModule();
+		await app.registerModule(myModule);
+		await app.init();
 
 		expect(myModule.app).to.be.equal(app);
 	});
@@ -278,7 +337,9 @@ describe('App', () => {
 			}
 		}
 
-		const app = new App().registerModule(new MyModule());
+		const app = new App();
+		await app.registerModule(new MyModule());
+
 		await expect(app.init()).to.be.rejected;
 		expect(app.getStatus()).to.be.equal('error');
 	});
@@ -291,7 +352,8 @@ describe('App', () => {
 			}
 		}
 
-		const app = new App().registerModule(new MyModule());
+		const app = new App();
+		await app.registerModule(new MyModule());
 		const module = await app.getModuleById('myModule');
 
 		expect(module).to.be.instanceof(MyModule);
@@ -310,7 +372,8 @@ describe('App', () => {
 			}
 		}
 
-		const app = new App().registerModule(new MyModule());
+		const app = new App();
+		await app.registerModule(new MyModule());
 		app.init();
 		const module = await app.getModuleById<MyModule>('myModule', true);
 
