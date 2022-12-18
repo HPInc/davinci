adaa9cc6f8cd78365ad233784419263087466fa1
Piero Maltese <maltesepiero@gmail.com>
2022-12-18 11:50:14 +0100

    chore: improved examples

--- examples/api-entity-manipulation/src/lib/createResourceListResponseEntity.ts
+++ examples/api-entity-manipulation/src/lib/createResourceListResponseEntity.ts
@@ -3,15 +3,10 @@
  * SPDX-License-Identifier: MIT
  */
 
-import { ClassType, DecoratorId, reflect } from '@davinci/reflector';
-import { entity, EntityOptions } from '@davinci/core';
+import { ClassType } from '@davinci/reflector';
+import { entity } from '@davinci/core';
 
 export function createResourceListResponseEntity<T>(entityClass: ClassType<T>) {
-	const reflection = reflect(entityClass);
-	const entityDecorator: { options: EntityOptions } = reflection.decorators.find(d => d[DecoratorId] === 'entity');
-	const entityName = entityDecorator?.options?.name ?? reflection.name;
-
-	@entity({ name: `${entityName}ListResponse` })
 	class ResourceListResponse {
 		@entity.prop({ type: [entityClass] })
 		data: Array<T>;
