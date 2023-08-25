a90cb4a1dd9746d6f936d2b4191f101f47077fd6
Piero Maltese <maltesepiero@gmail.com>
2023-08-25 13:35:55 +0100

    chore(polyfill): for node < 18

--- packages/http-server-hono/src/index.ts
+++ packages/http-server-hono/src/index.ts
@@ -2,7 +2,7 @@
  * © Copyright 2022 HP Development Company, L.P.
  * SPDX-License-Identifier: MIT
  */
-
+import './fetch-polyfill';
 import { Hono } from 'hono';
 
 export * from './HonoHttpServer';
